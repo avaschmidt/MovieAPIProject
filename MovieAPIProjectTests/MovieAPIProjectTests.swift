72b49f25e4b7dfefebae614165821ab3a4ced103
avaschmidt <ava.schmidt25@d155.org>
2025-01-14 07:49:44 -0600

    Initial Commit

---
+++ MovieAPIProjectTests/MovieAPIProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  MovieAPIProjectTests.swift
+//  MovieAPIProjectTests
+//
+//  Created by AVA SCHMIDT on 1/14/25.
+//
+
+import Testing
+@testable import MovieAPIProject
+
+struct MovieAPIProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
