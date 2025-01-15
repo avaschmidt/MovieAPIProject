5a6a0e84f32647908128ef4a4d43bf3bae8a1d5e
avaschmidt <ava.schmidt25@d155.org>
2025-01-15 07:56:03 -0600

    start

--- MovieAPIProject/ViewController.swift
+++ MovieAPIProject/ViewController.swift
@@ -1,19 +1,26 @@
-//
-//  ViewController.swift
-//  MovieAPIProject
-//
-//  Created by AVA SCHMIDT on 1/14/25.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var ghostDateOutlet: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        getMovie()
     }
-
-
+    // key: f36e3324
+    func getMovie(){
+        let session = URLSession.shared
+        let movieURL = URL(string: "http://www.omdbapi.com/?i=tt3896198&apikey=f36e3324")!
+        let dataTask = session.dataTask(with: movieURL) { data, response, error in
+            if let e = error{
+                print("Error! \(e)")
+            }
+            else{
+                if let d = data{
+                    
+                }
+            }
+        }
+        
+    }
 }
-
